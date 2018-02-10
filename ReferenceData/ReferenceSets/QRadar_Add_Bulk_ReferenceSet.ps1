9f0c66dfbd7735bf3cf53922dcf3cf1e1dde3a1f
Stahler <stah06@OSUMC.EDU>
2018-02-10 13:12:47 -0500

    cosmetic edit

--- ReferenceData/ReferenceSets/QRadar_Add_Bulk_ReferenceSet.ps1
+++ ReferenceData/ReferenceSets/QRadar_Add_Bulk_ReferenceSet.ps1
@@ -6,7 +6,6 @@ $ip = $ini['SIEM']['IP']
 $key = $ini['SIEM']['KEY']
 
 $body = ('100.100.100.6', '100.100.100.7', '100.100.100.8', '100.100.100.9') | ConvertTo-Json -Compress
-$body
 
 $params = @{
     uri                  = "https://$ip/api/reference_data/sets/bulk_load/DEMO_IP"
@@ -15,9 +14,7 @@ $params = @{
     body                 = $body
     SkipCertificateCheck = $true
     ContentType          = 'application/json'
-
 }
 
-$params
 $json_data = Invoke-RestMethod @params
 $json_data
