470a3ede7cb9399ebe49f4a991ca7faae2047901
Stahler <stah06@OSUMC.EDU>
2018-02-12 10:13:05 -0500

    Add reference map create - PowerShell

---
+++ ReferenceData/ReferenceMaps/QRadar_Create_ReferenceMap.ps1
@@ -0,0 +1,20 @@
+Import-Module -Name PsIni
+
+$path = 'C:\Users\stah06\Documents\QRadar\config.ini'
+$ini = Get-IniContent -FilePath $path
+$ip = $ini['SIEM']['IP']
+$key = $ini['SIEM']['KEY']
+
+$params = @{
+    uri                  = "https://$ip/api/reference_data/maps?"
+    headers              = @{'SEC' = $key }
+    body                 = @{
+        element_type     = 'ALNIC'
+        name             = 'DEMO_MAP2'
+    }
+    Method               = 'Post'
+    SkipCertificateCheck = $true
+}
+
+$json_data = Invoke-RestMethod @params
+$json_data
