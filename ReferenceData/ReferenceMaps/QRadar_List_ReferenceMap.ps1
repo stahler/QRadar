29eab415992785fecf30cc2ab9975b0c3e1b6fb8
Wes Stahler <wes.stahler@gmail.com>
2018-02-13 09:17:22 -0500

    Added PowerShell list example

---
+++ ReferenceData/ReferenceMaps/QRadar_List_ReferenceMap.ps1
@@ -0,0 +1,16 @@
+Import-Module -Name PsIni
+
+$path = 'C:\Users\stah06\Documents\QRadar\config.ini'
+$ini = Get-IniContent -FilePath $path
+$ip = $ini['SIEM']['IP']
+$key = $ini['SIEM']['KEY']
+
+$params = @{
+    uri                  = "https://$ip/api/reference_data/maps/DEMO_MAP2"
+    headers              = @{'SEC' = $key }
+    Method               = 'Get'
+    SkipCertificateCheck = $true
+}
+
+$json_data = Invoke-RestMethod @params
+$json_data
